28ed0e0d38d15caf778ee4e58e2dcf9764872d33
nikhilkharade <nikhilkharade070898@gmail.com>
2020-06-26 19:03:27 +0530

    duh

--- app.py
+++ app.py
@@ -20,10 +20,11 @@ def recieve_message(client_socket):
     try:
         message_header = client_socket.recv(HEADER_LENGTH)
         if not len(message_header):
-            return false
+            return False
         
         message_length = int(message_header.decode("utf-8").strip())
         return {"header" : message_header , "data" : client_socket.recv(message_length)}
     
-    try:
+    except:
+    	return False
    
